5ad730985e1bd8b44d58c451a432acbc4d201b87
Soichiro Kashima <soichiro.kashima@gmail.com>
2016-03-07 23:46:45 +0900

    Move Note* to note package

--- src/main/java/com/github/ksoichiro/note/web/note/NoteController.java
+++ src/main/java/com/github/ksoichiro/note/web/note/NoteController.java
@@ -1,4 +1,4 @@
-package com.github.ksoichiro.note.web;
+package com.github.ksoichiro.note.web.note;
 
 import com.github.ksoichiro.note.entity.Note;
 import com.github.ksoichiro.note.service.NoteService;
@@ -13,23 +13,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/note")
 public class NoteController {
     @Autowired
     private NoteService noteService;
 
-    @RequestMapping
+    @RequestMapping("/")
     public String list(Model model) {
         model.addAttribute("list", noteService.findAll());
         return "note/list";
     }
 
-    @RequestMapping("/create")
+    @RequestMapping("/note/create")
     public String create(@ModelAttribute NoteForm form) {
         return "note/create";
     }
 
-    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    @RequestMapping(value = "/note/save", method = RequestMethod.POST)
     public String save(@ModelAttribute NoteForm form, BindingResult bindingResult, Model model) {
         if (bindingResult.hasErrors()) {
             return create(form);
@@ -37,16 +36,16 @@ public class NoteController {
         Note entity = new Note();
         BeanUtils.copyProperties(form, entity);
         noteService.create(entity);
-        return "redirect:/note";
+        return "redirect:/";
     }
 
-    @RequestMapping("/edit/{id}")
+    @RequestMapping("/note/edit/{id}")
     public String edit(@PathVariable Integer id, @ModelAttribute NoteForm form) {
         BeanUtils.copyProperties(noteService.find(id), form);
         return "note/edit";
     }
 
-    @RequestMapping(value = "/update/{id}", method = RequestMethod.POST)
+    @RequestMapping(value = "/note/update/{id}", method = RequestMethod.POST)
     public String update(@PathVariable Integer id, @ModelAttribute NoteForm form, BindingResult bindingResult, Model model) {
         if (bindingResult.hasErrors()) {
             return edit(id, form);
@@ -55,6 +54,6 @@ public class NoteController {
         BeanUtils.copyProperties(form, entity);
         entity.setId(id);
         noteService.update(entity);
-        return "redirect:/note";
+        return "redirect:/";
     }
 }
--- src/main/java/com/github/ksoichiro/note/web/note/NoteForm.java
+++ src/main/java/com/github/ksoichiro/note/web/note/NoteForm.java
@@ -1,4 +1,4 @@
-package com.github.ksoichiro.note.web;
+package com.github.ksoichiro.note.web.note;
 
 import lombok.Data;
 import org.hibernate.validator.constraints.NotEmpty;
